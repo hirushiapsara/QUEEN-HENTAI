25a65bfb9438e65f769c07e944f77ac51e8a2298
hirushiapsara <134768143+hirushiapsara@users.noreply.github.com>
2024-02-01 08:37:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94757807978', 'Shamika', true],
+  ['94757807978', 'Shamika', true], 
   [''] 
 ] //Number of owners
 
